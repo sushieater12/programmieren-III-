ff9f7f2b8de1dbc16e495719558763e030a4c48e
sushieater12 <sushieater.12@tumo.world>
2024-01-16 18:30:13 +0100

    oh got zweitausend transferjahre später session 3

---
+++ session 3/experimente.js
@@ -0,0 +1,13 @@
+let obj = {
+    "first_name": "Vardan",
+    "last_name": "Hovsepyan",
+    "age": 13,
+    "tumo_student": true,
+sayHello(){
+        console.log("hello moin wie gehts, ziemlich schlecht wenn du dein altes projekt öffnest denke ich mal")
+    }
+
+ }
+ console.log(obj)
+ console.log("I am", obj.first_name)
+ obj.sayHello()
